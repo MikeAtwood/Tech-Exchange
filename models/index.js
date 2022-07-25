4d095ff01bf0287223bd262dd1c5e9305726d13d
Deadpool1 <atwood169@yahoo.com>
2022-07-25 11:10:18 -0500

    created belongsTo in index

--- models/index.js
+++ models/index.js
@@ -5,13 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
+Category.belongsTo(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  as: 'product_tag',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: 'product_tag',
+  id: 'product_tag',
+  foreignKey: 'product_id'
+})
 module.exports = {
   Product,
   Category,
